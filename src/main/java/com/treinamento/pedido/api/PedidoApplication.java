3650c60f378c396832eb59549dfeb0e23b3221a0
marcos.olivio <marcos.olivio@azi.com.br>
2018-07-11 16:27:48 -0400

    primeiro commit

---
+++ src/main/java/com/treinamento/pedido/api/PedidoApplication.java
@@ -0,0 +1,12 @@
+package com.treinamento.pedido.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedidoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PedidoApplication.class, args);
+    }
+}
